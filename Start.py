152bb0e198ad003b40cca483e0df3e8553f1ed3a
Nihilate <Nihilate@users.noreply.github.com>
2015-11-04 14:44:21 +1100

    Fixed weird bug where requests would timeout and crash everything

--- Start.py
+++ Start.py
@@ -4,8 +4,9 @@ import Config
 bot = YugiohLinkBot(Config.subredditlist)
 
 try:
-    bot.run()
-except BaseException as e:
+    while(1):
+        bot.run()
+except Exception as e:
     print("Shutting down bot: " + str(e))
     try:
         raise e
